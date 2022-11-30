6bce1b3cb764fb0cf309c0d617812b4a5ecb882b
asus <asus@MM18371>
2022-11-30 18:24:09 +0530

    jacoco

---
+++ Test/src/test/java/cseb/Test/AppTest.java
@@ -0,0 +1,23 @@
+package cseb.Test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppTest 
+{
+    @Test
+    public void test() {
+
+        App p = new App();
+        assertEquals("Hello DURGA", p.getMessage("DURGA"));
+
+    }
+    @Test
+    public void testNameEmpty() {
+
+        App c = new App();
+        assertEquals("Please provide a name!", c.getMessage(" "));
+
+    }
+}
